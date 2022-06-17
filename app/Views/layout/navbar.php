6cb967ecbbce2af246a4e27fbb913fc841ae638d
rahmadadeakbar <rahmadadeakbar@gmail.com>
2022-06-17 08:37:52 +0700

    login menu

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -21,6 +21,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/orang">Orang</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">Login</a>
+                </li>
             </ul>
         </div>
     </div>
